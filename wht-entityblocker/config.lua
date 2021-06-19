237a504e351b81ffd6ba24769e92a47237a0ac32
WH1TE-36 <80413469+WH1TE-36@users.noreply.github.com>
2021-06-19 16:52:17 +0200

    %30 Fivem Anticheat
    %30 Fivem Anticheat

---
+++ wht-entityblocker/config.lua
@@ -0,0 +1,102 @@
+
+Config = {}
+
+Config.Webhook = "WEBHOOK GIR"
+
+Config.DropMessage = "WHT-EntityBlocker Sg Karsim"
+
+Config.AntiSpawnVehicles = true
+
+Config.WhitelistedObjects = {
+    1760825203,
+    413312110,
+  }
+
+  Config.WhitelistedPeds = {
+    0x780C01BD,
+  }
+
+  Config.BlacklistedPeds = {
+    0x780C01BD,
+  }
+
+  Config.BlacklistedVehicles = { -- Yasaklı Araçlar
+  	"adder",
+	"cerberus",
+	"cerberus2",
+	"cerberus3",
+	"hauler2",
+	"phantom2",
+	"terbyte",
+	"issi4",
+	"issi5",
+	"issi6",
+	"akula",
+	"annihilator",
+	"buzzard",
+	"hunter",
+	"savage",
+	"apc",
+	"barrage",
+	"chernobog",
+	"halftVCACk",
+	"khanjali",
+	"minitank",
+	"rhino",
+	"scarab",
+	"scarab2",
+	"scarab3",
+	"thruster",
+	"trailersmall2",
+	"oppressor",
+	"oppressor2",
+	"dominator4",
+	"dominator5",
+	"dominator6",
+	"dukes2",
+	"impaler2",
+	"impaler3",
+	"impaler4",
+	"imperator",
+	"imperator2",
+	"imperator3",
+	"bruiser",
+	"bruiser2",
+	"bruiser3",
+	"brutus",
+	"brutus2",
+	"brutus3",
+	"caVCACara",
+	"dune4",
+	"dune5",
+	"insurgent",
+	"insurgent3",
+	"menacer",
+	"nightshark",
+	"technical",
+	"technical2",
+	"technical3",
+	"zhaba",
+	"avenger",
+	"avenger2",
+	"besra",
+	"blimp",
+	"blimp2",
+	"blimp3",
+	"jet",
+	"hydra",
+	"strikeforce",
+	"titan",
+	"volatol",
+	"limo2",
+	"zr380",
+	"zr3802",
+	"zr3803",
+	"deluxo",
+	"vigilante",
+	"boxville5",
+	"kuruma2",
+	"scramjet",
+	"rcmpaparazzo_2",
+}
+
---
+++ wht-entityblocker/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "adamant"
+
+game "gta5"
+
+author 'WHT-AntiEntitySpawner'
+
+client_scripts {
+    "client/*.lua",
+    "config.lua"
+}
+
+server_scripts {
+    "server/*.lua",
+    "config.lua"
+}
